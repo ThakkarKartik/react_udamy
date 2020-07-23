e53af1f2c502e41a6af191e2e57541d6b0998db3
Kartik Thakkar <thakkar.kartik2011@gmail.com>
2020-07-24 00:04:45 +0530

    Simple Login with Redux

--- APP/Components/Login/Actions.js
+++ APP/Components/Login/Actions.js
@@ -1,70 +1,69 @@
-//import * as types from './ActionTypes'
-//import Store from '../../Store'
-import React from 'react'
-import {NavigationActions, StackActions} from 'react-navigation';
-//import { Home } from '../Home/Home'
-import { HOME,PROFILE } from '../../Navigation/RoutePaths'
+import { NavigationActions, StackActions } from 'react-navigation';
+import { HOME, PROFILE } from '../../Navigation/RoutePaths'
 import * as actionTypes from './ActionTypes'
 import { Alert } from 'react-native';
 
 export const onPress = () => {
-    return dispatch => {
-        dispatch(
-          NavigationActions.navigate({
-            routeName:HOME,
-            // params: {
-            //   passValue: value,
-            // },
-          }),
-        );
-      };
+  return dispatch => {
+    dispatch(
+      NavigationActions.navigate({
+        routeName: HOME,
+        // params: {
+        //   passValue: value,
+        // },
+      }),
+    );
+  };
 };
-export const LoginPress = (email,pass) =>{
-  
-  response = CheckLogin(email, pass);
-  Alert.alert("response","-> "+ response)
-  return async dispatch =>{
-    onPressDispatch(dispatch, email, pass);  
+export const LoginPress = (email, pass) => {
+  if (email == "Kt" && pass == "Kt") {
+    return async dispatch => {
+      onPressDispatch(dispatch, email, pass);
     }
-
   }
-  const CheckLogin = (email, pass) =>
-  {
-    return fetch('https://reqres.in/api/login')
+  else {
+    Alert.alert("response", "Wrong Email or Password")
+  }
+  //  response = CheckLogin(email, pass);
+  // Alert.alert("response","-> "+ response)
+
+}
+const CheckLogin = (email, pass) => {
+  return fetch('https://reqres.in/api/login')
     .then((response) => response.json())
-    .then((json)=>{
-      return json;
+    .then((response) => {
+      return response;
     })
     .catch((error) => {
       console.error(error);
     });
+}
+const onPressDispatch = (dispatch, email, pass) => {
+  dispatch(
+    NavigationActions.navigate({
+      routeName: PROFILE,
+      
+    }),
+  );
+}
+export const onTextChange = (input, type) => {
+  console.log('onTextChange', input + '-' + type);
+  switch (type) {
+    case 'email':
+      return {
+        type: actionTypes.INPUT_USER_EMAIL,
+        input: input,
+      };
+    case 'password':
+      return {
+        type: actionTypes.INPUT_USER_PASSWORD,
+        input: input,
+      };
+    default:
+      break;
   }
-  const onPressDispatch = (dispatch,email,pass) =>
-  {
-    dispatch(
-      NavigationActions.navigate({
-        routeName:PROFILE,
-      }),
-    );
-  }
-  export const onTextChange = (input, type) => {
-    console.log('onTextChange', input + '-' + type);
-    switch (type) {
-      case 'email':
-        return {
-          type: actionTypes.INPUT_USER_EMAIL,
-          input: input,
-        };
-      case 'password':
-        return {
-          type: actionTypes.INPUT_USER_PASSWORD,
-          input: input,
-        };
-      default:
-        break;
-    }
-  };
-  
+};
+
 
   //return true;    
 
